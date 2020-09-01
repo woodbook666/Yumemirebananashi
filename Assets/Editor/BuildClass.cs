73c8ae3190e0c52d4871e82a9aa2b40083409b43
ReNeeter <reneeter123@gmail.com>
2020-09-01 21:11:57 +0900

    ビルドスクリプトを追加

---
+++ Assets/Editor/BuildClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System.Linq;
+
+public class BuildClass : MonoBehaviour
+{
+    public static void Winx64LZ4HCBuild()
+    {
+        string[] scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(s => s.path).ToArray();
+        BuildReport buildReport = BuildPipeline.BuildPlayer(scenes, "build/StandaloneWindows64", BuildTarget.StandaloneWindows64, BuildOptions.CompressWithLz4HC);
+    }
+}
